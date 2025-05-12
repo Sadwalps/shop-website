2774944981768d67bb4fced097b4b8b1ff8630e8
Sadwalps <sadwalps2255@gmail.com>
2025-05-12 19:53:52 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"" , component:HomeComponent , title:"Home page"
+    },
+    {
+        path:"about" , component:AboutComponent , title:"About"
+    },
+    {
+        path:"products" , component:ProductComponent , title:"Products"
+    },
+    {
+        path:"contact" , component:ContactComponent , title:"Contact"
+    }
+    
+];
